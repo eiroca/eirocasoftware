b966382f932d6ac3a60edd32b7154da85ad9c16a
umibozu <umibozu@3fdb2778-5978-40a7-a8af-66ec70f38830>
2010-01-04 22:29:52 +0000

    barcode DokuWiki plugin added: - Google Chart as provider - CSS Style handling - Removed align (now with CSS)

--- trunk/Vario/DokuWiki/plugin/barcode/lang/en/settings.php
+++ trunk/Vario/DokuWiki/plugin/barcode/lang/en/settings.php
@@ -1,3 +1,4 @@
 <?php
 $lang['showfooter'] = 'Hint to download 2Dbarcode mobile reader?';
-$lang['provider'] = 'Provider for the barcode generator (0=Kaywa, 1=i-nigma)';
+$lang['provider'] = 'Provider for the barcode generator (0=Google, 1=Kaywa, 2=i-nigma)';
+?>
