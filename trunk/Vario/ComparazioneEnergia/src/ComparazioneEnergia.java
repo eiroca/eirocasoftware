65326d809a59486fb1f3508c3badeabf3c4a0798
umibozu <umibozu@3fdb2778-5978-40a7-a8af-66ec70f38830>
2010-02-26 22:05:51 +0000

    GPL3

--- trunk/Vario/ComparazioneEnergia/src/ComparazioneEnergia.java
+++ trunk/Vario/ComparazioneEnergia/src/ComparazioneEnergia.java
@@ -1,15 +1,25 @@
-import java.io.BufferedReader;
+/**
+ * Copyright (C) 1998-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ * 
+ */
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.util.List;
 import java.util.StringTokenizer;
-import net.htmlparser.jericho.Element;
-import net.htmlparser.jericho.Source;
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
 
