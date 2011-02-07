45cb50d076695b6c222b96acc10be70864b205e5
umibozu <umibozu@3fdb2778-5978-40a7-a8af-66ec70f38830>
2011-02-07 14:43:52 +0000

    Updated barcode plugin

--- site/www.eiroca.net/wiki/lib/plugins/barcode/syntax.php
+++ site/www.eiroca.net/wiki/lib/plugins/barcode/syntax.php
@@ -2,42 +2,17 @@
 /**
  * Plugin barcode: 2D-Barcode Implementation
  * @author Enrico Croce & Simona Burzio (staff@eiroca.net)
- * @copyright Copyright (C) 2009-2010 eIrOcA - Enrico Croce & Simona Burzio
+ * @copyright Copyright (C) 2009-2011 eIrOcA - Enrico Croce & Simona Burzio
  * @license GPL >=3 (http://www.gnu.org/licenses/)
- * @version 1.0.0
+ * @version 1.0.2
  * @link http://www.eiroca.net
  */
 if (!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if (!defined('DOKU_PLUGIN_BARCODE')) define('DOKU_PLUGIN_BARCODE',DOKU_PLUGIN.'barcode/');
 require_once (DOKU_PLUGIN.'syntax.php');
-class QRProvider {
-	function addParam(&$first, $param) {
-		if (!$first) { return "&".$param; }
-		$first = false;
-		return $param;
-	}
-	function _IMG($url,$id,$class) {
-		$out = '<img src="'.$url.'" alt="barcode"';
-		if ($id) {$out .= ' id="'.$id.'"';}
-		if ($class) {$out .= ' class="'.$class.'"';}
-		$out .= ' />';
-		return $out;
-	}
-	function render(&$p) {
-		return "";
-	}
-}
+require_once (DOKU_PLUGIN_BARCODE.'api/barcode.inc');
 class syntax_plugin_barcode extends DokuWiki_Syntax_Plugin {
-	function getInfo() {
-		return array (
-			'author'=>'eIrOcA',
-			'email'=>'staff@eiroca.net',
-			'date'=>'20010-04-20',
-			'name'=>'barcode -- 2D-Barcode Plugin',
-			'url'=>'http://www.eiroca.net',
-			'desc'=>'2D-Barcode Plugin. Syntax: ~~BARCODE~id=css_id~class=css_class~mode=0~size=M~caption=x~url=http://myurl.com~~'
-			);
-	}
 	function getType() { return 'substition'; }
 	function getPType() { return 'normal'; }
 	function getSort() { return 999; }
@@ -77,9 +52,7 @@ class syntax_plugin_barcode extends DokuWiki_Syntax_Plugin {
 			}
 		}
 		$provider = $this->getConf('provider');
-		require_once DOKU_PLUGIN."barcode/api/$provider/api.inc";
-		$provider = "QR_".$provider;
-		$service = new $provider();
+		$service = QRProvider::getService($provider);
 		$out .= $service->render($p);
 		return $out;
 	}
@@ -91,9 +64,9 @@ class syntax_plugin_barcode extends DokuWiki_Syntax_Plugin {
 		if ($mode == 'xhtml') {
 			$renderer->doc .= $data;
 			if ($this->getConf('showfooter')) {
-				$lang=$conf["lang"];
-				$fn = DOKU_PLUGIN . "barcode/footer_".$lang.".txt";
-				if (!file_exists($fn)) $fn = DOKU_PLUGIN . 'barcode/footer.txt';
+				$lang = $conf["lang"];
+				$fn = DOKU_PLUGIN_BARCODE."footer_".$lang.".txt";
+				if (!file_exists($fn)) $fn = DOKU_PLUGIN_BARCODE."footer.txt";
 				$renderer->doc .= @file_get_contents($fn);
 			}
 			return true;
